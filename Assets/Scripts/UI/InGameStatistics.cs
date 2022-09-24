53a9031eb6ddc5b5412cc37e937312f57305a846
Max Kersten <max-kersten@live.nl>
2022-09-25 01:19:48 +0200

    [ / ] Map restructure

--- Assets/Scripts/UI/InGameStatistics.cs
+++ Assets/Scripts/UI/InGameStatistics.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class UserInterfaceManager : MonoBehaviour
+public class InGameStatistics : MonoBehaviour
 {
     private GameManager gameManager;
 
